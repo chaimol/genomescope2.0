ec3f05e36529348684ec543be462a71df8943f8b
chaimol <chaimol@163.com>
2022-04-20 10:33:45 +0800

    update install methods

--- install.R
+++ install.R
@@ -1,5 +1,6 @@
 local_lib_path = "~/R_libs/"
+if (!dir.exists(local_lib_path)){dir.create(local_lib_path)}
 
-install.packages('minpack.lm', lib=local_lib_path, repos = "http://cran.us.r-project.org")
-install.packages('argparse', lib=local_lib_path, repos = "http://cran.us.r-project.org")
-install.packages('.', repos=NULL, type="source", lib=local_lib_path)
+if(!require("minpack.lm")){install.packages("minpack.lm",lib=local_lib_path)}
+if(!require("argparse")){install.packages("argparse",lib=local_lib_path)}
+if(!require("genomescope")){install.packages('.', repos=NULL, type="source", lib=local_lib_path)}
